9c49e30cb494dfd690651c6fad29d48e9947f345
SmgTR <smagokpnd@gmail.com>
2023-06-17 09:57:35 +0200

    add navigation with semantic ui

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,8 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true 
+pin "semantic-ui-sass", to: "https://ga.jspm.io/npm:semantic-ui-sass@2.4.2/semantic-ui.js", preload: true
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
