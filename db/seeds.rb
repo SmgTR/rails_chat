63bf6419f088431650cae78e0aa1ff07ef8a0194
SmgTR <smagokpnd@gmail.com>
2023-07-20 12:11:12 +0200

    seed users to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(username: "John", password: "password1")
+User.create(username: "Thomas", password: "password1")
+User.create(username: "Sara", password: "password1")
