2c9df2239c2026179ae3940af91f3e210a23fb52
SmgTR <smagokpnd@gmail.com>
2023-07-20 12:05:52 +0200

    build message

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,15 +3,21 @@ import 'controllers';
 import 'jquery';
 import 'semantic-ui-sass';
 
-$(function () {
+const semanticUi = () => {
   $('.ui.accordion').accordion();
 
   $('.ui.dropdown').dropdown();
   $('.ui.menu .item').tab();
   $('.menu .item').tab();
   $('.tiny.menu .item').tab();
-});
-$('.ui.button').on('click', function () {
-  // programmatically activating tab
-  $.tab('change tab', 'tab-name');
-});
+  $('.ui.button').on('click', function () {
+    // programmatically activating tab
+    $.tab('change tab', 'tab-name');
+  });
+  $('.message .close').on('click', function () {
+    $(this).closest('.message').transition('fade');
+  });
+};
+
+$(document).on('turbo:load', semanticUi);
+import "channels"
